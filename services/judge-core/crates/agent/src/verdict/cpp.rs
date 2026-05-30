a57c823443bb457efc2d807fbf1e08db85097955
xinhallow <xinhallow@outlook.com>
2026-05-30 16:43:07 +0800

    feat: 实现 Cpp 处理器的 prepare、compile 和 cleanup 方法

--- services/judge-core/crates/agent/src/verdict/cpp.rs
+++ services/judge-core/crates/agent/src/verdict/cpp.rs
@@ -1,14 +1,59 @@
+use std::{path::PathBuf, process::Stdio, time::Duration};
+
+use tokio::{fs, io::AsyncReadExt, process, time::timeout};
+
 use crate::verdict::Verdict;
 
-pub struct Cpp {}
+pub struct Cpp {
+    work_dir: PathBuf,
+}
 
 impl Verdict for Cpp {
-    async fn prepare(workdir: &std::path::Path, id: u64) -> Result<Self, super::VerdictError> {
-        todo!()
+    async fn prepare(workdir: &std::path::Path, _id: u64) -> Result<Self, super::VerdictError> {
+        fs::create_dir_all(workdir).await?;
+
+        Ok(Self {
+            work_dir: workdir.to_path_buf(),
+        })
     }
 
     async fn compile(&self, source: &str) -> Result<super::CompileResult, super::VerdictError> {
-        todo!()
+        let source_path = self.work_dir.join("source.cpp");
+        fs::write(&source_path, source).await?;
+
+        let mut cmd = process::Command::new("g++");
+        cmd.arg("-std=c++23")
+            .arg("-w")
+            .arg(source_path.display().to_string())
+            .arg("-o")
+            .arg("executable")
+            .current_dir(&self.work_dir)
+            .stdin(Stdio::piped())
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped());
+
+        // TODO: Apply resource limits
+
+        let mut child = cmd.spawn()?;
+        let result = timeout(Duration::from_secs(10), child.wait()).await;
+        match result {
+            Ok(status) => {
+                let status = status?;
+                if status.success() {
+                    Ok(super::CompileResult::Success)
+                } else {
+                    let mut message = String::new();
+                    if let Some(mut stderr) = child.stderr.take() {
+                        stderr.read_to_string(&mut message).await?;
+                    }
+                    Ok(super::CompileResult::CompilationError { message })
+                }
+            }
+            Err(_) => {
+                child.kill().await?;
+                Ok(super::CompileResult::Timeout)
+            }
+        }
     }
 
     async fn verdict(
@@ -20,6 +65,8 @@ impl Verdict for Cpp {
     }
 
     async fn cleanup(&self) -> Result<(), super::VerdictError> {
-        todo!()
+        fs::remove_dir_all(&self.work_dir).await?;
+
+        Ok(())
     }
 }
