89c283c8518338fb0e07f5972fb391d2af934b31
xinhallow <xinhallow@outlook.com>
2026-06-19 17:36:29 +0800

    feat: 实现接受请求并处理的逻辑

---
+++ services/submission-processor/src/main.ts
@@ -0,0 +1,47 @@
+import { connect } from "amqplib";
+import type { VerdictTask } from "judge-core-sdk";
+import { JudgeCoreClient } from "judge-core-sdk";
+import { pino } from "pino";
+
+const logger = pino({ name: "submission-processor" });
+
+const SUBMIT_QUEUE = "submit.queue";
+const RESULT_QUEUE = "result.queue";
+const SUBMIT_ROUTE = "submit";
+const RESULT_ROUTE = "result";
+const EXCHANGE_NAME = "online-judge.exchange";
+
+const connection = await connect(process.env.RABBIT_MQ_URL!);
+logger.info("connected rabbitmq");
+
+const channel = await connection.createChannel();
+await channel.assertExchange(EXCHANGE_NAME, "direct", { durable: true });
+await channel.assertQueue(SUBMIT_QUEUE, { durable: true });
+await channel.assertQueue(RESULT_QUEUE, { durable: true });
+await channel.bindQueue(SUBMIT_QUEUE, EXCHANGE_NAME, SUBMIT_ROUTE);
+await channel.bindQueue(RESULT_QUEUE, EXCHANGE_NAME, RESULT_ROUTE);
+await channel.prefetch(1);
+logger.info("configured rabbit mq");
+
+const client = new JudgeCoreClient(process.env.JUDGE_CORE_URL!);
+
+await channel.consume(
+	SUBMIT_QUEUE,
+	async (msg) => {
+		if (!msg) return;
+
+		const task: VerdictTask = JSON.parse(msg.content.toString());
+		const result = await client.submitTask(task);
+
+		channel.publish(
+			EXCHANGE_NAME,
+			RESULT_ROUTE,
+			Buffer.from(JSON.stringify(result)),
+		);
+
+		logger.info("finished task");
+
+		channel.ack(msg);
+	},
+	{ noAck: true },
+);
