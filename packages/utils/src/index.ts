d32ceab69330345a0b7b3d2c93424936e73f1990
xinhallow <xinhallow@outlook.com>
2026-06-19 18:08:30 +0800

    refactor: 将初始化连接 rabbit mq 的逻辑迁移到 utils 包

---
+++ packages/utils/src/index.ts
@@ -0,0 +1,31 @@
+import { connect } from "amqplib";
+
+export const initRabbitMq = async (connectionUrl: string) => {
+	const SUBMIT_QUEUE = "submit.queue";
+	const RESULT_QUEUE = "result.queue";
+	const SUBMIT_ROUTE = "submit";
+	const RESULT_ROUTE = "result";
+	const EXCHANGE_NAME = "online-judge.exchange";
+
+	const connection = await connect(connectionUrl);
+
+	const channel = await connection.createChannel();
+	await channel.assertExchange(EXCHANGE_NAME, "direct", { durable: true });
+	await channel.assertQueue(SUBMIT_QUEUE, { durable: true });
+	await channel.assertQueue(RESULT_QUEUE, { durable: true });
+	await channel.bindQueue(SUBMIT_QUEUE, EXCHANGE_NAME, SUBMIT_ROUTE);
+	await channel.bindQueue(RESULT_QUEUE, EXCHANGE_NAME, RESULT_ROUTE);
+	await channel.prefetch(1);
+
+	return {
+		channel,
+		connection,
+		config: {
+			SUBMIT_QUEUE,
+			RESULT_QUEUE,
+			SUBMIT_ROUTE,
+			RESULT_ROUTE,
+			EXCHANGE_NAME,
+		},
+	};
+};
--- services/submission-processor/src/main.ts
+++ services/submission-processor/src/main.ts
@@ -1,34 +1,19 @@
-import { connect } from "amqplib";
 import type { VerdictTask } from "judge-core-sdk";
 import { JudgeCoreClient } from "judge-core-sdk";
 import { pino } from "pino";
+import { initRabbitMq } from "utils";
 
 const logger = pino({ name: "submission-processor" });
 
-const SUBMIT_QUEUE = "submit.queue";
-const RESULT_QUEUE = "result.queue";
-const SUBMIT_ROUTE = "submit";
-const RESULT_ROUTE = "result";
-const EXCHANGE_NAME = "online-judge.exchange";
-
-const connection = await connect(process.env.RABBIT_MQ_URL!);
-logger.info("connected rabbitmq");
-
-const channel = await connection.createChannel();
-await channel.assertExchange(EXCHANGE_NAME, "direct", { durable: true });
-await channel.assertQueue(SUBMIT_QUEUE, { durable: true });
-await channel.assertQueue(RESULT_QUEUE, { durable: true });
-await channel.bindQueue(SUBMIT_QUEUE, EXCHANGE_NAME, SUBMIT_ROUTE);
-await channel.bindQueue(RESULT_QUEUE, EXCHANGE_NAME, RESULT_ROUTE);
-await channel.prefetch(1);
-logger.info("configured rabbit mq");
+const { channel, config } = await initRabbitMq(process.env.RABBIT_MQ_URL!);
+logger.info("configured rabbitmq");
 
 const client = new JudgeCoreClient(process.env.JUDGE_CORE_URL!);
 await client.getAcceptablez();
 logger.info("judge-core available");
 
 await channel.consume(
-	SUBMIT_QUEUE,
+	config.SUBMIT_QUEUE,
 	async (msg) => {
 		if (!msg) return;
 
@@ -36,8 +21,8 @@ await channel.consume(
 		const result = await client.submitTask(task);
 
 		channel.publish(
-			EXCHANGE_NAME,
-			RESULT_ROUTE,
+			config.EXCHANGE_NAME,
+			config.RESULT_ROUTE,
 			Buffer.from(JSON.stringify(result)),
 		);
 
