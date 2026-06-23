86e697770cf38dc43f93e521cf69a3bc3f8d0fba
xinhallow <xinhallow@outlook.com>
2026-06-23 22:34:44 +0800

    feat(apps/api.v2): Add `Swagger` support

--- apps/api.v2/src/main.ts
+++ apps/api.v2/src/main.ts
@@ -1,9 +1,19 @@
 import { Module } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 @Module({})
 class RootModule {}
 
 const app = await NestFactory.create(RootModule);
+
+const config = new DocumentBuilder()
+	.setTitle("TATEN Online Judge Platform")
+	.setDescription("The platform API references")
+	.setVersion("0.1.0-unstable")
+	.addTag("contest")
+	.build();
+const documentFactory = () => SwaggerModule.createDocument(app, config);
+SwaggerModule.setup("api", app, documentFactory);
 
 await app.listen(3699);
