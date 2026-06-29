e09e205da35922cc8815a0da2a0a2399dfa28bc0
xinhallow <xinhallow@outlook.com>
2026-06-29 22:00:11 +0800

    refactor: Rename `Difficulty` variants

--- services/crates/api_server_db/src/models/enums.rs
+++ services/crates/api_server_db/src/models/enums.rs
@@ -7,25 +7,25 @@ use serde::{Deserialize, Serialize};
 pub enum Difficulty {
     /// 入门
     #[serde(rename = "入门")]
-    A = 0,
+    Beginner = 0,
     /// 普及-
     #[serde(rename = "普及-")]
-    B = 1,
+    PopularizeMinus = 1,
     /// 普及/提高-
     #[serde(rename = "普及/提高-")]
-    C = 2,
+    PopularizeImprove = 2,
     /// 普及+/提高
     #[serde(rename = "普及+/提高")]
-    D = 3,
+    PopularizePlus = 3,
     /// 提高+/省选-
     #[serde(rename = "提高+/省选-")]
-    E = 4,
+    ImprovePlus = 4,
     /// 省选/NOI-
     #[serde(rename = "省选/NOI-")]
-    F = 5,
+    Provincial = 5,
     /// NOI/NOI+/CTSC
     #[serde(rename = "NOI/NOI+/CTSC")]
-    G = 6,
+    Noi = 6,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, EnumIter, DeriveActiveEnum, Serialize, Deserialize)]
