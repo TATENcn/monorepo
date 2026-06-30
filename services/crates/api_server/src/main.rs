da4c91df308a0fda316bda51973ec15900134dbb
xinhallow <xinhallow@outlook.com>
2026-06-30 22:35:03 +0800

    feat: Add problems routes to root router

--- services/crates/api_server/src/main.rs
+++ services/crates/api_server/src/main.rs
@@ -1,8 +1,13 @@
 use std::sync::Arc;
 
-use api_server::{ApiServerError, config::ApiServerConfig};
+use api_server::{
+    ApiServerError,
+    config::ApiServerConfig,
+    routers::{ApiServerState, problems},
+};
+use api_server_db::repositories::{problems::ProblemsRepo, test_cases::TestCasesRepo};
 use auth::router::{jwks_router, router};
-use axum::Router;
+use axum::{Extension, Router};
 use sea_orm::Database;
 use tokio::net::TcpListener;
 use tracing::{error, info};
@@ -21,9 +26,16 @@ async fn main() -> Result<(), ApiServerError> {
     let auth_state = Arc::new(config.to_auth_app_state(connection.clone()).await?);
 
     let listener = TcpListener::bind("localhost:3080").await?;
+    let api_state = Arc::new(ApiServerState {
+        problems_repo: ProblemsRepo::new(connection.clone()),
+        test_cases_repo: TestCasesRepo::new(connection.clone()),
+        db: connection.clone(),
+    });
     let router = Router::new()
         .nest("/api/v1/auth", router(auth_state.clone()))
-        .nest("/.well-known", jwks_router(auth_state));
+        .nest("/api/v1/problems", problems::router().with_state(api_state))
+        .nest("/.well-known", jwks_router(auth_state.clone()))
+        .layer(Extension(auth_state));
 
     axum::serve(listener, router)
         .with_graceful_shutdown(async {
