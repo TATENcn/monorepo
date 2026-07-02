4c4d1a52b31f622ea73f2d74f9fa51e941f54635
xinhallow <xinhallow@outlook.com>
2026-07-02 22:39:12 +0800

    refactor(services/api_server): Remove auth routes

--- services/crates/api_server/src/config.rs
+++ services/crates/api_server/src/config.rs
@@ -1,14 +1,9 @@
-use auth::router::AppState as AuthAppState;
 use config::{Config, ConfigError, Environment, File};
-use sea_orm::DatabaseConnection;
 use serde::{Deserialize, Serialize};
-use tokio::{fs, io};
 
 #[config_macro::config]
 #[derive(Debug, Deserialize, Serialize)]
 pub struct ApiServerConfig {
-    #[serde(default)]
-    pub auth: AuthConfig,
     #[serde(default)]
     pub database: DatabaseConfig,
 }
@@ -18,40 +13,6 @@ pub struct ApiServerConfig {
 pub struct DatabaseConfig {
     #[config_val(default = "postgresql://postgres:postgres@localhost:5432/taten".into())]
     pub database_url: String,
-}
-
-#[config_macro::config]
-#[derive(Debug, Deserialize, Serialize)]
-pub struct AuthConfig {
-    /// Private key pen filepath
-    pub private_key_pem_filepath: String,
-    /// Public key pen filepath
-    pub public_key_pem_filepath: String,
-    /// Access token lifetime in seconds
-    #[config_val(default = 900)]
-    pub access_token_ttl_secs: u64,
-    /// Refresh token lifetime in seconds
-    #[config_val(default = 7 * 24 * 60 * 60)]
-    pub refresh_token_ttl_secs: u64,
-}
-
-impl AuthConfig {
-    pub fn validate(&self) -> Result<(), &'static str> {
-        if self.private_key_pem_filepath.is_empty() {
-            return Err("private key pem filepath cannot be empty");
-        }
-        if self.public_key_pem_filepath.is_empty() {
-            return Err("private key pem filepath cannot be empty");
-        }
-        if self.access_token_ttl_secs == 0 {
-            return Err("access token lifetime cannot be 0");
-        }
-        if self.refresh_token_ttl_secs == 0 {
-            return Err("refresh token lifetime cannot be 0");
-        }
-
-        Ok(())
-    }
 }
 
 const DEFAULT_CONFIG_PATH: &str = "./config/api_server.toml";
@@ -68,18 +29,5 @@ impl ApiServerConfig {
             .build()?;
 
         config.try_deserialize()
-    }
-
-    pub async fn to_auth_app_state(&self, db: DatabaseConnection) -> Result<AuthAppState, io::Error> {
-        let private = fs::read(&self.auth.private_key_pem_filepath).await?;
-        let public = fs::read(&self.auth.public_key_pem_filepath).await?;
-
-        Ok(AuthAppState {
-            db,
-            private_key_pem: private,
-            public_key_pem: public,
-            access_token_ttl_secs: self.auth.access_token_ttl_secs,
-            refresh_token_ttl_secs: self.auth.refresh_token_ttl_secs,
-        })
     }
 }
--- services/crates/api_server/src/main.rs
+++ services/crates/api_server/src/main.rs
@@ -6,24 +6,18 @@ use api_server::{
     routers::{ApiServerState, problems},
 };
 use api_server_db::repositories::{problems::ProblemsRepo, test_cases::TestCasesRepo};
-use auth::router::{jwks_router, router};
-use axum::{Extension, Router};
+use axum::Router;
 use sea_orm::Database;
 use tokio::net::TcpListener;
-use tracing::{error, info};
+use tracing::info;
 
 #[tokio::main]
 async fn main() -> Result<(), ApiServerError> {
     tracing_subscriber::fmt::init();
 
     let config = ApiServerConfig::load()?;
-    if let Err(msg) = config.auth.validate() {
-        error!("{msg}");
-        return Err(ApiServerError::Config(config::ConfigError::Message(msg.into())));
-    }
     info!(?config, "configuration loaded");
     let connection = Database::connect(&config.database.database_url).await?;
-    let auth_state = Arc::new(config.to_auth_app_state(connection.clone()).await?);
 
     let listener = TcpListener::bind("localhost:3080").await?;
     let api_state = Arc::new(ApiServerState {
@@ -31,11 +25,7 @@ async fn main() -> Result<(), ApiServerError> {
         test_cases_repo: TestCasesRepo::new(connection.clone()),
         db: connection.clone(),
     });
-    let router = Router::new()
-        .nest("/api/v1/auth", router(auth_state.clone()))
-        .nest("/api/v1/problems", problems::router().with_state(api_state))
-        .nest("/.well-known", jwks_router(auth_state.clone()))
-        .layer(Extension(auth_state));
+    let router = Router::new().nest("/api/v1/problems", problems::router().with_state(api_state));
 
     axum::serve(listener, router)
         .with_graceful_shutdown(async {
